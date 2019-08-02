02f57b840959bdd0972aec210cd9c3e038c271f5
xing.lin <xinglin@china.com>
2019-08-02 11:59:49 +0800

    指南针框架springboot mybatis

---
+++ src/main/java/com/hydsoft/compass/CompassApplication.java
@@ -0,0 +1,13 @@
+package com.hydsoft.compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompassApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hydsoft/compass/controller/ApiController.java
@@ -0,0 +1,33 @@
+package com.hydsoft.compass.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("openApi")
+/**  
+* @title: ApiController.java
+* @description: Test对接接口
+* @author xingl
+* @date 2019/5/27 16:49
+*/
+public class ApiController {
+    //日志句柄
+    private static final Logger LOGGER = LogManager.getLogger(ApiController.class);
+
+    /**
+     * Test对接接口
+     * @param
+     * @return
+     */
+    @RequestMapping(method =  RequestMethod.POST,value = "/duerosApi")
+    public void test() {
+        System.out.println("Test***********************");
+    }
+
+}
